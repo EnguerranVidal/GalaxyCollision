bce5ea034499696fd93af967c032d1f73d0c50b1
KeplerDream <engue11@hotmail.com>
2022-07-04 02:52:20 +0200

    Changing few tests functions
    - Adding Cupy detection system for faster GPU computation
    - Adding velocity distribution graph (Non Functional)
    - Adapted simulator inputs in relation to change in inputs in sources/simulators of last commit

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-from sources.old.__galaxycollision import *
-
 from sources.galaxies import *
 from sources.engines import *
 from sources.simulators import *
+from sources.common.graphs import *
+
+try:
+    import cupy as np
+    print('CUPY VERSION DETECTED')
+except ImportError:
+    import numpy as np
 
 
-def main():
-    """ Main program """
-    # GALAXY 1 : Main Galaxy
-    rings1 = np.linspace(0.5, 3, 20)
-    particles1 = np.full_like(rings1, 40)
-    gal1 = RingsMasslessGalaxy(rings1, particles1, 10000, 5)
-    gal1.initialState([0, 0], [0, 0])
+def testGeneration():
+    galaxy = DiskGalaxy2D('MILKY_WAY', 1000, 1, 75, mass=1)
+    plot2DVelDistribution(galaxy)
 
-    # GALAXY 2 : Dwarf Galaxy Incoming
-    X, V = initial_trajectory(3, 0.6, -135, gal1.centralMass)
-    # We send the dwarf galaxy on a collision course along an elliptic conic
-    rings2 = np.linspace(0.1, 1, 15)
-    particles2 = np.full_like(rings2, 20)
-    gal2 = RingsMasslessGalaxy(rings2, particles2, 500, 2.5)
-    gal2.initialState(X, V)
 
-    # SIMULATION
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    merger = Galaxy_Collision([gal1, gal2], current_dir)
-    merger.RUN(0.01, 6, method='Runge_Kutta')  # We run some calculations
-    merger.display(gif_fps=25, gif_duration=4)  # We create a GIF from the showcased Matplotlib animation
+def singleGalaxyTest():
+    execDir = os.path.dirname(os.path.abspath(__file__))
+    clusters = [DiskGalaxy2D('MILKY_WAY', 1000, 1, 75, mass=1)]
+    sim = Simulator(clusters, execDir, mode='BARNES_HUT')
+    sim.run(0.1, 50, method='EULER_SEMI_IMPLICIT')
+    createGIF2D(sim.sessionPath, size=5)
+
+
+def testCreationGIF():
+    path = 'D:\\_PROJECTS\\PYTHON PROJECTS\\Galaxy_Merger\\logs\\20220630224442'
+    createGIF2D(path, size=(100, 90))
 
 
 if __name__ == "__main__":
-    main()
+    testGeneration()
