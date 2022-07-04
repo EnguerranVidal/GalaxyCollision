5a3f38175e80e48e56ffc1b8032cd494bfec60fb
KeplerDream <engue11@hotmail.com>
2022-07-04 02:27:46 +0200

    Changing OctTree
    - Setiing up OctTree for future fixes.
    - Preparing for 3D engine

--- sources/engines.py
+++ sources/engines.py
@@ -6,95 +6,79 @@ import threading
 
 
 class OctTree:
-    def __init__(self, massesX, massesM, theta, limits=None):
-        if limits is None:
-            xMax = np.max(np.abs(massesX[:, 0])) * 1.01
-            yMax = np.max(np.abs(massesX[:, 1])) * 1.01
-            zMax = np.max(np.abs(massesX[:, 2])) * 1.01
-            limits = (-xMax, xMax, -yMax, yMax, -zMax, zMax)
-        if not type(massesX).__module__ == np.__name__:
-            massesX = np.array([massesX])
-        if not type(massesM).__module__ == np.__name__:
-            massesM = np.array([massesM])
-        self.massesX = massesX
-        self.massesM = massesM
-        self.gravitationCst = gravitationalConstant()
-        self.nbObjects = self.massesX.shape[0]
+    def __init__(self, theta, limits=None, softLength=1/10**4):
         self.children = [None, None, None, None, None, None, None, None]
+        self.massesX = None
+        self.massesM = None
+        self.gravitationCst = gravitationalConstant()
+        self.nbObjects = 0
         self.x1, self.x2 = limits[0], limits[1]
         self.y1, self.y2 = limits[2], limits[3]
         self.z1, self.z2 = limits[4], limits[5]
-        self.centreMassX = None
-        self.centreMass = None
         self.theta = theta
-        self.getCenter()
-        self.buildChildren()
+        self.softLength = softLength
+        # Mass Center
+        self.centreMassX = 0
+        self.centreMass = np.array([0, 0, 0])
 
     def getCenter(self):
-        xc = np.sum(self.massesX[:, 0])
-        yc = np.sum(self.massesX[:, 1])
-        zc = np.sum(self.massesX[:, 2])
-        mc = np.sum(self.massesM)
-        self.centreMass = mc
-        self.centreMassX = np.array([xc, yc, zc])
+        Mc = np.sum(self.massesM)
+        self.centreMass = Mc
+        if Mc != 0:
+            Rc = np.array([0.0, 0.0, 0.0])
+            for i in range(self.nbObjects):
+                Rc += self.massesM[i] * self.massesX[i, :]
+            self.centreMassX = Rc / Mc
 
-    def buildChildren(self):
+    def insert(self, position, mass):
         # Top : towards +Z, North : towards +Y, East : towards +X
         # Each list :  contains positions [0] and masses [1]
-        TNW, TNE, TSW, TSE = [[], []], [[], []], [[], []], [[], []]
-        BNW, BNE, BSW, BSE = [[], []], [[], []], [[], []], [[], []]
         # Calculating middle points
         mx = (self.x1 + self.x2) / 2
         my = (self.y1 + self.y2) / 2
         mz = (self.z1 + self.z2) / 2
         # Filling children recursively
-        for i in range(self.nbObjects):
-            if self.massesX[i, 0] > mx:  # East
-                if self.massesX[i, 1] > my:  # North
-                    if self.massesX[i, 2] > mz:  # Top
-                        TNE[0].append(self.massesX[i, :])
-                        TNE[1].append(self.massesM[i])
+        ###### ADDING PARTICLE TO SELF ######
+        if self.nbObjects == 0:
+            self.massesM = np.array([mass])
+            self.massesX = np.array([position])
+        else:
+            self.children = [OctTree(self.theta, (mx, self.x2, my, self.y2, mz, self.z2)),  # TNE
+                             OctTree(self.theta, (mx, self.x2, my, self.y2, self.z1, mz)),  # BNE
+                             OctTree(self.theta, (mx, self.x2, self.y1, my, mz, self.z2)),  # TSE
+                             OctTree(self.theta, (mx, self.x2, self.y1, my, self.z1, mz)),  # BSE
+                             OctTree(self.theta, (self.x1, mx, my, self.y2, mz, self.z2)),  # BNW
+                             OctTree(self.theta, (self.x1, mx, my, self.y2, self.z1, mz)),  # BNW
+                             OctTree(self.theta, (self.x1, mx, self.y1, my, mz, self.z2)),  # BSW
+                             OctTree(self.theta, (self.x1, mx, self.y1, my, self.z1, mz))]  # BSW
+            self.massesM = np.append(self.massesM, np.array([mass]), 0)
+            self.massesX = np.vstack([self.massesX, position])
+            ###### ADDING PARTICLE TO CHILDREN ######
+            if position[0] > mx:  # East
+                if position[1] > my:  # North
+                    if position[2] > mz:  # Top
+                        self.children[0].insert(position, mass)
+                    else:                 # Bottom
+                        self.children[1].insert(position, mass)
+                else:                 # South
+                    if position[2] > mz:  # Top
+                        self.children[2].insert(position, mass)
+                    else:                 # Bottom
+                        self.children[3].insert(position, mass)
+            else:                 # West
+                if position[1] > my:  # North
+                    if position[2] > mz:  # Top
+                        self.children[4].insert(position, mass)
                     else:  # Bottom
-                        BNE[0].append(self.massesX[i, :])
-                        BNE[1].append(self.massesM[i])
-                else:  # South
-                    if self.massesX[i, 2] > mz:  # Top
-                        TSE[0].append(self.massesX[i, :])
-                        TSE[1].append(self.massesM[i])
+                        self.children[5].insert(position, mass)
+                else:                 # South
+                    if position[2] > mz:  # Top
+                        self.children[6].insert(position, mass)
                     else:  # Bottom
-                        BSE[0].append(self.massesX[i, :])
-                        BSE[1].append(self.massesM[i])
-            else:  # West
-                if self.massesX[i, 1] > my:  # North
-                    if self.massesX[i, 2] > mz:  # Top
-                        TNW[0].append(self.massesX[i, :])
-                        TNW[1].append(self.massesM[i])
-                    else:  # Bottom
-                        BNW[0].append(self.massesX[i, :])
-                        BNW[1].append(self.massesM[i])
-                else:  # South
-                    if self.massesX[i, 2] > mz:  # Top
-                        TSW[0].append(self.massesX[i, :])
-                        TSW[1].append(self.massesM[i])
-                    else:  # Bottom
-                        BSW[0].append(self.massesX[i, :])
-                        BSW[1].append(self.massesM[i])
-        if len(TNE[0]) > 0:
-            self.children[0] = OctTree(TNE[0], TNE[1], self.theta, (mx, self.x2, my, self.y2, mz, self.z2))
-        if len(BNE[0]) > 0:
-            self.children[1] = OctTree(BNE[0], BNE[1], self.theta, (mx, self.x2, my, self.y2, self.z1, mz))
-        if len(TSE[0]) > 0:
-            self.children[2] = OctTree(TSE[0], TSE[1], self.theta, (mx, self.x2, self.y1, my, mz, self.z2))
-        if len(BSE[0]) > 0:
-            self.children[3] = OctTree(BSE[0], BSE[1], self.theta, (mx, self.x2, self.y1, my, self.z1, mz))
-        if len(TNW[0]) > 0:
-            self.children[4] = OctTree(TNW[0], TNW[1], self.theta, (self.x1, mx, my, self.y2, mz, self.z2))
-        if len(BNW[0]) > 0:
-            self.children[5] = OctTree(BNW[0], BNW[1], self.theta, (self.x1, mx, my, self.y2, self.z1, mz))
-        if len(TSW[0]) > 0:
-            self.children[6] = OctTree(TSW[0], TSW[1], self.theta, (self.x1, mx, self.y1, my, mz, self.z2))
-        if len(BSW[0]) > 0:
-            self.children[7] = OctTree(BSW[0], BSW[1], self.theta, (self.x1, mx, self.y1, my, self.z1, mz))
+                        self.children[7].insert(position, mass)
+        ###### RECALCULATING CENTRE OF MASS ######
+        self.nbObjects += 1
+        self.getCenter()
 
     def forces(self, position):
         if not type(position).__module__ == np.__name__:
@@ -105,12 +89,12 @@ class OctTree:
         if distance != 0:
             ratio = width / distance
             if ratio < self.theta or self.allNoneChildren():
-                acceleration = (self.gravitationCst * self.centreMass * vector) / (distance ** 3)
+                acceleration = (self.gravitationCst * self.centreMass * vector) / (distance + self.softLength) ** 3
                 return acceleration
             else:
-                acceleration = np.array([0, 0, 0])
+                acceleration = np.array([0.0, 0.0, 0.0])
                 for i in self.children:
-                    if isinstance(i, OctTree):
+                    if isinstance(i, OctTree) and i.nbObjects != 0:
                         acceleration += i.forces(position)
         else:
             return np.array([0, 0, 0])
